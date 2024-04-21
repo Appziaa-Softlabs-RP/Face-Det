98e17a738ef4700df5de02f11d5ee09c86d2ebb7
= <=>
2024-04-21 18:05:34 +0530

    updated

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -12,6 +12,6 @@ class Authenticate extends Middleware
      */
     protected function redirectTo(Request $request): ?string
     {
-        return $request->expectsJson() ? null : response()->json(['message', 'Unauthenticated'], 403);
+        return $request->expectsJson() ? null : response()->json(['message', 'Unauthenticated']);
     }
 }
