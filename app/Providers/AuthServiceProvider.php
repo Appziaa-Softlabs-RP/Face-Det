59ef959bb77ffdd9c71ea85dab8aad9e4b967aae
Manu Kumar <65483269+mannu24@users.noreply.github.com>
2024-04-29 16:25:08 +0530

    Update: Removed token expiry

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -22,6 +22,6 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        Passport::personalAccessTokensExpireIn(now()->addMinutes(30));
+        // Passport::personalAccessTokensExpireIn(now()->addMinutes(30));
     }
 }
